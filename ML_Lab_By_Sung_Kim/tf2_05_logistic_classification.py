e03021ce456d6850f02f581706bafc67c84ce3db
Jisu Choi <redgem92@gmail.com>
2021-04-26 12:18:00 +0000

    Softmax classification

---
+++ ML_Lab_By_Sung_Kim/tf2_05_logistic_classification.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import tensorflow as tf
+
+x_data = [[1, 2],
+          [2, 3],
+          [3, 1],
+          [4, 3],
+          [5, 3],
+          [6, 2]]
+y_data = [[0],
+          [0],
+          [0],
+          [1],
+          [1],
+          [1]]
+
+tf.model = tf.keras.Sequential()
+tf.model.add(tf.keras.layers.Dense(units=1, input_dim=2))
+# Use sigmoid activation for 0~1 problem
+tf.model.add(tf.keras.layers.Activation('sigmoid'))
+# Better result with loss function == 'binary_crossentropy'
+# Adding acurracy metric to get accuracy report during training
+# binary_crossentropy not recommended, use mse
+tf.model.compile(loss='mse', optimizer=tf.keras.optimizers.SGD(lr=0.01), metrics=['accuracy'])
+tf.model.summary()
+
